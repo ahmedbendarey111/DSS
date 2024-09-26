18152af1433f0f5c4f43bb50eb0aefd35fd17f8f
ahmedbendarey111 <ahmedbendarey111@gmail.com>
2024-09-26 21:08:20 +0300

    Update DSS.py

--- DSS.py
+++ DSS.py
@@ -26,6 +26,8 @@ df.columns  = [i.upper() for i in df.columns]
 #df['DATE']=df['DATE'].str.split(' ').str[0]
 #df['DATE']= pd.to_datetime(df['DATE'])
 #df.dropna(axis=0, inplace=True)
+df['RESULT']=df['RESULT'].replace('1','Verification Code is Valid')
+
 df['CERTIFICATE NO']=df['CERTIFICATE NO'].astype('str')
 Result=df[df['CERTIFICATE NO']==SN]
 
